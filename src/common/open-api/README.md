f94d460ff8fe31f4b81df75afb9a3c1ff76facd0
Nick Tomlin <NickTomlin@users.noreply.github.com>
2022-05-13 13:44:04 -0500

    chore: Bootstrap open api

---
+++ src/common/open-api/README.md
@@ -0,0 +1,11 @@
+These models were bootstrapped [`open-api-generator`](https://openapi-generator.tech/)
+
+```shell
+openapi-generator generate -g typescript-fetch -i https://play-app.orkes.io/api-docs
+```
+
+# Changes
+
+The `runtime` file has been modified to:
+
+
---
+++ src/common/open-api/apis/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminResourceApi';
+export * from './ApplicationResourceApi';
+export * from './AuthorizationResourceApi';
+export * from './EventResourceApi';
+export * from './GroupResourceApi';
+export * from './HealthCheckResourceApi';
+export * from './MetadataResourceApi';
+export * from './MigrationResourceApi';
+export * from './QueueAdminResourceApi';
+export * from './SchedulerResourceApi';
+export * from './TaskResourceApi';
+export * from './TokenResourceApi';
+export * from './UserResourceApi';
+export * from './WorkflowBulkResourceApi';
+export * from './WorkflowResourceApi';
