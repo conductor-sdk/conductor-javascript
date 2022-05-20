52ec9f43084aa5147e272efae7690f6e5ab63941
Nick Tomlin <NickTomlin@users.noreply.github.com>
2022-05-20 14:45:24 -0500

    chore: Make request customizer functional instead of needing a class

--- src/common/RequestCustomizer.ts
+++ src/common/RequestCustomizer.ts
@@ -1,4 +1,7 @@
-import {NodeHttpRequest} from "./open-api/core/NodeHttpRequest"
+import {ApiRequestOptions} from "./open-api/core/ApiRequestOptions"
+import {CancelablePromise} from "./open-api/core/CancelablePromise"
+import {OpenAPIConfig} from "./open-api/core/OpenAPI"
+import {request as baseRequest} from "./open-api/core/request"
 
 /**
  * Useful for customizing the behavior of `http` calls using the `ConductorClient`.
@@ -6,14 +9,13 @@ import {NodeHttpRequest} from "./open-api/core/NodeHttpRequest"
  * @remarks
  * Customizing the request URL
  * ```
- * class Customizer extends RequestCustomizer {
- *  request(options) {
- *    const url = options.url.replace(/^\/api/, '')
- *    return super.request({...options, url });
- *  }
- *}
+ *
+ * const requestCustomizer = (request, config, options) =>  {
+ *  const url = options.url.replace(/^\/api/, '')
+ *  return request(config, {...options, url });
+ * }
  * const config = { BASE: "https://my-api.com"}
- * const client = new ConductorClient(config, Customizer)
+ * const client = new ConductorClient(config, requestCustomizer)
  * ```
  */
-export class RequestCustomizer extends NodeHttpRequest {}
+export type ConductorHttpRequest = <T>(request: typeof baseRequest, config: OpenAPIConfig, options: ApiRequestOptions) => CancelablePromise<T>
