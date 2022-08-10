b6c33b32e754e4be878e864180d2b76a987b4a9b
James Stuart Milne <jstuartmilne@gmail.com>
2022-08-10 10:49:39 -0300

    fix: inputParameter spelling

--- src/common/generators/KafkaTask.ts
+++ src/common/generators/KafkaTask.ts
@@ -1,6 +1,13 @@
 import { KafkaPublishTaskDef, TaskType } from "../types";
 import { nameTaskNameGenerator } from "./common";
 
+/**
+ * Takes an optional partial KafkaPublishTaskDef
+ * generates a task replacing default/fake values with provided overrides
+ *
+ * @param overrides overrides for defaults
+ * @returns a fully defined task
+ */
 export const generateKafkaPublishTask = (
   overrides: Partial<KafkaPublishTaskDef> = {}
 ): KafkaPublishTaskDef => ({
@@ -12,7 +19,7 @@ export const generateKafkaPublishTask = (
       bootStrapServers: "localhost:9092",
       headers: {},
       key: "123",
-      keySerializser: "org.apache.kafka.common.serialization.IntegerSerializer",
+      keySerializer: "org.apache.kafka.common.serialization.IntegerSerializer",
     },
   },
   ...overrides,
