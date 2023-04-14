60171ec8cc32c7f60cd5c2eab8fa8471467d10c9
gardusig <gustavo.gardusi@gmail.com>
2023-04-14 09:14:44 -0300

    Deleted unnecessary services under ConductorClient

--- src/common/open-api/ConductorClient.ts
+++ src/common/open-api/ConductorClient.ts
@@ -4,23 +4,12 @@
 import type { BaseHttpRequest } from "./core/BaseHttpRequest";
 import type { OpenAPIConfig, Resolver } from "./core/OpenAPI";
 
-import { AdminResourceService } from "./services/AdminResourceService";
-import { ApplicationResourceService } from "./services/ApplicationResourceService";
-import { AuthorizationResourceService } from "./services/AuthorizationResourceService";
 import { EventResourceService } from "./services/EventResourceService";
-import { GroupResourceService } from "./services/GroupResourceService";
 import { HealthCheckResourceService } from "./services/HealthCheckResourceService";
 import { MetadataResourceService } from "./services/MetadataResourceService";
-import { MigrationResourceService } from "./services/MigrationResourceService";
-import { PublisherConfigResourceService } from "./services/PublisherConfigResourceService";
-import { QueueAdminResourceService } from "./services/QueueAdminResourceService";
 import { SchedulerResourceService } from "./services/SchedulerResourceService";
-import { SecretResourceService } from "./services/SecretResourceService";
-import { TagsExperimentalService } from "./services/TagsExperimentalService";
 import { TaskResourceService } from "./services/TaskResourceService";
 import { TokenResourceService } from "./services/TokenResourceService";
-import { UserResourceService } from "./services/UserResourceService";
-import { VersionResourceService } from "./services/VersionResourceService";
 import { WorkflowBulkResourceService } from "./services/WorkflowBulkResourceService";
 import { WorkflowResourceService } from "./services/WorkflowResourceService";
 import { request as baseRequest } from "./core/request";
@@ -39,23 +28,12 @@ export interface ConductorClientAPIConfig extends Omit<OpenAPIConfig, "BASE"> {
 }
 
 export class ConductorClient {
-  public readonly adminResource: AdminResourceService;
-  public readonly applicationResource: ApplicationResourceService;
-  public readonly authorizationResource: AuthorizationResourceService;
   public readonly eventResource: EventResourceService;
-  public readonly groupResource: GroupResourceService;
   public readonly healthCheckResource: HealthCheckResourceService;
   public readonly metadataResource: MetadataResourceService;
-  public readonly migrationResource: MigrationResourceService;
-  public readonly publisherConfigResource: PublisherConfigResourceService;
-  public readonly queueAdminResource: QueueAdminResourceService;
   public readonly schedulerResource: SchedulerResourceService;
-  public readonly secretResource: SecretResourceService;
-  public readonly tagsExperimental: TagsExperimentalService;
   public readonly taskResource: TaskResourceService;
   public readonly tokenResource: TokenResourceService;
-  public readonly userResource: UserResourceService;
-  public readonly versionResource: VersionResourceService;
   public readonly workflowBulkResource: WorkflowBulkResourceService;
   public readonly workflowResource: WorkflowResourceService;
 
@@ -93,25 +71,13 @@ export class ConductorClient {
     // END conductor-client-modification
     this.token = config?.TOKEN;
 
-    this.adminResource = new AdminResourceService(this.request);
-    this.applicationResource = new ApplicationResourceService(this.request);
-    this.authorizationResource = new AuthorizationResourceService(this.request);
+
     this.eventResource = new EventResourceService(this.request);
-    this.groupResource = new GroupResourceService(this.request);
     this.healthCheckResource = new HealthCheckResourceService(this.request);
     this.metadataResource = new MetadataResourceService(this.request);
-    this.migrationResource = new MigrationResourceService(this.request);
-    this.publisherConfigResource = new PublisherConfigResourceService(
-      this.request
-    );
-    this.queueAdminResource = new QueueAdminResourceService(this.request);
     this.schedulerResource = new SchedulerResourceService(this.request);
-    this.secretResource = new SecretResourceService(this.request);
-    this.tagsExperimental = new TagsExperimentalService(this.request);
     this.taskResource = new TaskResourceService(this.request);
     this.tokenResource = new TokenResourceService(this.request);
-    this.userResource = new UserResourceService(this.request);
-    this.versionResource = new VersionResourceService(this.request);
     this.workflowBulkResource = new WorkflowBulkResourceService(this.request);
     this.workflowResource = new WorkflowResourceService(this.request);
   }
