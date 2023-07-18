30e5ce740d3e664afec40268e858a671e6b72b47
James Stuart Milne <jstuartmilne@gmail.com>
2023-07-18 14:21:11 -0300

    refactor: tied to node-fetch

--- src/orkes/OrkesConductorClient.ts
+++ src/orkes/OrkesConductorClient.ts
@@ -1,15 +1,33 @@
 import { ConductorHttpRequest } from "../common";
+import { fetchCatchDns } from "./request/fetchCatchDns";
 import { request as baseRequest } from "./request/request";
 import { baseOrkesConductorClient } from "./BaseOrkesConductorClient";
 import { FetchFn } from "./types";
+import fetch, { Headers, RequestInit, Response } from "node-fetch";
 
-const fetchCache = require("./fetchImplementation");
+const nodeFetchWrapper: FetchFn<RequestInit, Response> = async (
+  input,
+  options = {}
+) => {
+  const res = await fetch(input.toString(), options as RequestInit);
+  return res;
+};
+
+const fetchCache =
+  fetchCatchDns<{ headers: Record<string, string> }, Response>(
+    nodeFetchWrapper,
+    {
+      //@ts-ignore
+      headerFactory: (headers?: HeadersInit) =>
+        new Headers((headers as Record<string, string>) || {}),
+    }
+  );
 
 const defaultRequestHandler: ConductorHttpRequest = (
   __request,
   config,
   options
-) => baseRequest(config, options, fetch);
+) => baseRequest(config, options, fetchCache as unknown as FetchFn);
 
 /**
  * Takes a config with keyId and keySecret returns a promise with an instance of ConductorClient
@@ -19,7 +37,6 @@ const defaultRequestHandler: ConductorHttpRequest = (
  * @returns
  */
 export const orkesConductorClient = baseOrkesConductorClient(
-  //@ts-ignore
-  fetchCache as FetchFn<RequestInit,Response>,
+  fetchCache,
   defaultRequestHandler
 );
