da8b37691e4aaa7414c31bf87f4961961fdb9486
James Stuart Milne <jstuartmilne@gmail.com>
2023-07-28 18:27:18 -0300

    feat: added keepalive agent

--- src/orkes/OrkesConductorClient.ts
+++ src/orkes/OrkesConductorClient.ts
@@ -4,24 +4,34 @@ import { request as baseRequest } from "./request/request";
 import { baseOrkesConductorClient } from "./BaseOrkesConductorClient";
 import { FetchFn } from "./types";
 import fetch, { Headers, RequestInit, Response } from "node-fetch";
+import http from "http";
+import https from "https";
+
+const httpAgent = new http.Agent({ keepAlive: true });
+const httpsAgent = new https.Agent({ keepAlive: true });
+
+const agent = (_parsedURL: URL) =>
+  _parsedURL.protocol == "http:" ? httpAgent : httpsAgent;
 
 const nodeFetchWrapper: FetchFn<RequestInit, Response> = async (
   input,
   options = {}
 ) => {
-  const res = await fetch(input.toString(), options as RequestInit);
+  const res = await fetch(input.toString(), {
+    ...options,
+    agent,
+  } as RequestInit);
   return res;
 };
 
-const fetchCache =
-  fetchCatchDns<{ headers: Record<string, string> }, Response>(
-    nodeFetchWrapper,
-    {
-      //@ts-ignore
-      headerFactory: (headers?: HeadersInit) =>
-        new Headers((headers as Record<string, string>) || {}),
-    }
-  );
+const fetchCache = fetchCatchDns<{ headers: Record<string, string> }, Response>(
+  nodeFetchWrapper,
+  {
+    //@ts-ignore
+    headerFactory: (headers?: HeadersInit) =>
+      new Headers((headers as Record<string, string>) || {}),
+  }
+);
 
 const defaultRequestHandler: ConductorHttpRequest = (
   __request,
